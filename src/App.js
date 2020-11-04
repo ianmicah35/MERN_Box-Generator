f18afe63741701519ed474a33607a86f3f3f8315
ianmicah35 <ianmicah35@gmail.com>
2020-11-04 17:28:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -14,12 +14,9 @@ function App() {
     <div className="App">
       <h1 className="jumbotron">Welcome to the Box Generator</h1>
       <div><BoxForm generateBox={generateBox}/></div>
-        <div className="box">
         {
           boxList.map((item, i) => <Box myBox={item} />)
         }
-      </div>
-
     </div>
   );
 }
